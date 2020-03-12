8105be42ee4b0eeb41d6e1e9c91ecfc171964f88
jhonier <jhonierjimenez99@gmail.com>
2020-03-11 23:06:12 -0500

    version 1.0

--- logica.js
+++ logica.js
@@ -5,18 +5,27 @@ $(function () {
     $('#u-tabla3').hide();
     $('#lpro').hide();
     $('#formEditar').hide();
+
+   
     agregarUsuario();
     agregarMascotas();
     listaPropietario();
     editarUsuario();
+    editarMascota();
     guardarEdicionUsuario();
     eliminar();
+    eliminarM();
+    ver();
 
 });
+
+//variable universal
+var permitido=1;
 
 function agregarUsuario() {
 
     $('#aggP').on('click', function (e) {
+        permitido=0;
         e.preventDefault();
         $('#formularioM').hide();
         $('#u-tabla2').hide();
@@ -34,85 +43,106 @@ function agregarUsuario() {
                                     + "<td>" + telefono.val() + "</td>"
                                     + "<td><button class='btn btn-primary caggM'>Agregar Mascotas</button></td>" +
                             "</tr>");
-        /*nombre.val(" ");
+        nombre.val(" ");
         apellido.val(" ");
         direccion.val(" ");
-        telefono.val(" ");*/
+        telefono.val(" ");
     });
 }
 
+
+
 function agregarMascotas() {
     
+    var tdApellido, tdNombre,nombrePersona,apellidoPerso;
     $('#u-tabla').on('click', '.caggM', function (e) {
         e.preventDefault();
-        alert("hola");
+        $("#formPropietario").hide();
         $('#formularioM').show();
         $('#u-tabla2').hide();
 
         var tr = $(this).closest('tr');
-        var tdNombre =   tr.children("td:nth-child(1)");
-        var tdApellido = tr.children("td:nth-child(2)");
+        tdNombre =   tr.children("td:nth-child(1)");
+        tdApellido = tr.children("td:nth-child(2)");
 
         //opteniendo informacion del dueño de la mascota
-        var nombrePersona = tdNombre.html();
-        var apellidoPerso = tdApellido.html();
+        nombrePersona = tdNombre.html();
+        apellidoPerso = tdApellido.html();
 
         //opteniendo informacion del formulario de mascotas
-        var nombremascota = $('#nombremascota');
-        var tipomascota = $('#tipomascota');
-        var fechamascota = $('#fechamascota');
-        
-
-        $("#aggM").on("click" ,function(e){
-            e.preventDefault();
-
-            $('#u-tabla2').append("<tr>"
-                                        + "<td>" + nombremascota.val() + "</td>"
-                                        + "<td>" + tipomascota.val()   + "</td>"
-                                        + "<td>" + fechamascota.val()  + "</td>"
-                                + "</tr>");
-
-            $('#u-tabla2').show();
-        });
 
         $("#npropietario").html(nombrePersona);
 
     });
+
+    var nombremascota = $('#nombremascota');
+    var tipomascota = $('#tipomascota');
+    var fechamascota = $('#fechamascota');
+
+    $("#aggM").on("click" , function(e){
+        //ocultar
+         
+         $("#formPropietario").show();
+         $("#formularioM").hide();
+
+         e.preventDefault();
+         $('#u-tabla2').append("<tr>"
+                                     + "<td>" + nombremascota.val() + "</td>"
+                                     + "<td>" + tipomascota.val()   + "</td>"
+                                     + "<td>" + fechamascota.val()  + "</td>"
+                                     + "<td>" + nombrePersona  + "</td>"
+                                    + "<td><button class='btn btn-primary editM'>Editar</button></td>" 
+
+                             + "</tr>");
+
+           $('#u-tabla2').show();
+           nombremascota.val(" ");
+           tipomascota.val(" ");
+           fechamascota.val(" ");
+          
+     });
 }
 
 function listaPropietario(){
-
-    $("#listar").on("click", function (){
-       $("#u-tabla3").show();
-       $("#txtagregarmascota").text("Lista de Usuarios");
-       $("#u-tabla").hide();
-       $("#formPropietario").hide();
-       $("#formularioM").hide();
-       $("#filaTablaMascota").hide();
-
-        var nombre, apellido, direccion,telefono;
-        var _row='';
-        var header_1='<tr> <td>Nombre</td> <td>Apellido</td> <td>Direccion</td> <td>Telefono</td> <td>Opciones</td></tr>';
         
+        $("#listar").on("click", function (){
+        //ocultar 
+            $("#u-tabla3").show();
+            $("#u-tabla2").show();
+            $("#txtagregarmascota").text("Lista de Usuarios");
+            $("#u-tabla").hide();
+            $("#formPropietario").hide();
+            $("#formularioM").hide();
+            $("#filaTablaMascota").hide();
 
-        $("#body_filtro").html('');
-        $("#body_filtro").html(header_1);
-        _row='';
-        $("#u-tabla tbody tr").each(function(idx, fila){
-                      
-           nombre=     fila.children[0].innerHTML;
-           apellido=   fila.children[1].innerHTML;
-           direccion=  fila.children[2].innerHTML;
-           telefono=   fila.children[3].innerHTML;
-           //nota=   fila.children[4].innerHTML;
+                var nombre, apellido, direccion,telefono;
+                var _row='';
+                var header_1='<tr> <td>Nombre</td> <td>Apellido</td> <td>Direccion</td> <td>Telefono</td> <td>Opciones</td></tr>';
+                
 
-           _row='<tr><td>'+nombre+'</td><td>'+apellido+'</td><td>'+direccion+'</td><td>'+telefono+'</td><td>'+"<a href='#' class='editar'>ver/editar</a>"+'</td></tr>';
+                $("#body_filtro").html('');
+                $("#body_filtro").html(header_1);
+                _row='';
+                $("#u-tabla tbody tr").each(function(idx, fila){
+                            
+                    nombre=     fila.children[0].innerHTML;
+                    apellido=   fila.children[1].innerHTML;
+                    direccion=  fila.children[2].innerHTML;
+                    telefono=   fila.children[3].innerHTML;
+                    //nota=   fila.children[4].innerHTML;
 
-           $("#u-tabla3").append(_row);
+                    _row='<tr><td>'+nombre+'</td><td>'+apellido+'</td><td>'+direccion+'</td><td>'+telefono+'</td><td>'+"<a href='#' class='editar'>Editar</a>"+'</td></tr>';
+                    if(permitido == 0){
+                       $("#u-tabla3").append(_row);
+                    }
 
-        });
-                        
-    });
+                    });
+
+                permitido=1;
+                                
+            });
+
+
 }
 
 function editarUsuario() {
@@ -128,16 +158,16 @@ function editarUsuario() {
         
 
         var vnombre = tdNombre.html();
-        tdNombre.html("<input type='text' id='txtNombre' />");
+        tdNombre.html("<input type='text' id='txtNombre' placeholder='Nombre' />");
 
         var vapellido = tdApellido.html();
-        tdApellido.html("<input type='text' id='txtApellido' />");
+        tdApellido.html("<input type='text' id='txtApellido' placeholder='Apellido' />");
 
         var vdireccion = tdDireccion.html();
-        tdDireccion.html("<input type='text' id='txtDireccion' />");
+        tdDireccion.html("<input type='text' id='txtDireccion' placeholder='Direccion' />");
 
         var vtelefono = tdTelefono.html();
-        tdTelefono.html("<input type='text' id='txtTelefono' />");
+        tdTelefono.html("<input type='number' id='txtTelefono' placeholder='Telefono' />");
 
 
         tdOpciones.html("<a href='#' class='guardar button'>Guardar</a>" +
@@ -146,11 +176,38 @@ function editarUsuario() {
 }
 
 
+function editarMascota() {
+    $('#u-tabla2').on('click', '.editM', function(e) {
+        e.preventDefault();
+
+        var tr =   $(this).closest('tr');
+        var tdNombre =     tr.children("td:nth-child(1)");
+        var tdTipo =   tr.children("td:nth-child(2)");
+        var tdFecha =  tr.children("td:nth-child(3)");
+        var tdOpciones =   tr.children("td:nth-child(5)");
+        
+
+        var vnombre = tdNombre.html();
+        tdNombre.html("<input type='text' id='txtNombre' placeholder='Nombre M' />");
+
+        var vtipo = tdTipo.html();
+        tdTipo.html("<input type='text' id='txtTipo' placeholder='tipo' />");
+
+        var vdireccion = tdFecha.html();
+        tdFecha.html("<input type='text' id='txtFecha' placeholder='fecha dd/mm/aaaa/' />");
+
+       
+
+        tdOpciones.html("<a href='#' class='guardarM button'>GuardarM</a>" +
+                        "<a href='#' class='eliminarM button'>EliminarM</a>");
+    });
+}
+
+
 function guardarEdicionUsuario() {
 
     $('#u-tabla3').on('click', '.guardar', function(e) {
         e.preventDefault();
-        alert("guardar")
 
         var tr = $(this).closest('tr');
         var tdNombre =      tr.children("td:nth-child(1)");
@@ -168,16 +225,50 @@ function guardarEdicionUsuario() {
         var nuevoDireccion = tdDireccion.children("input[type=text]").val();
         tdDireccion.html(nuevoDireccion);
 
-        var nuevoTelefono = tdTelefono.children("input[type=text]").val();
+        var nuevoTelefono = tdTelefono.children("input[type=number]").val();
         tdTelefono.html(nuevoTelefono);
 
-        tdOpciones.html("<a href='#' class='editar button'>ver/editar</a>");
+        tdOpciones.html("<a href='#' class='editar button'>Editar</a>");
     });
 }
 
 
+function guardarEdicionUsuario() {
+
+    $('#u-tabla2').on('click', '.guardarM', function(e) {
+        e.preventDefault();
+
+        var tr = $(this).closest('tr');
+        var tdNombre =      tr.children("td:nth-child(1)");
+        var tdTipo =    tr.children("td:nth-child(2)");
+        var tdFecha =   tr.children("td:nth-child(3)");
+        var tdOpciones =    tr.children("td:nth-child(5)");
+
+        var nuevoNombre = tdNombre.children("input[type=text]").val();
+        tdNombre.html(nuevoNombre);
+
+        var nuevoTipo = tdTipo.children("input[type=text]").val();
+        tdTipo.html(nuevoTipo);
+
+        var nuevoFecha = tdFecha.children("input[type=date]").val();
+        tdFecha.html(nuevoFecha);
+
+
+        tdOpciones.html("<a href='#' class='editM button'>EditarM</a>");
+    });
+}
+
+
+
 function eliminar() {
     $('#u-tabla3').on('click', '.eliminar', function(e) {
+        e.preventDefault();
+        $(this).closest('tr').remove();
+    });
+}
+
+function eliminarM() {
+    $('#u-tabla2').on('click', '.eliminarM', function(e) {
         e.preventDefault();
         $(this).closest('tr').remove();
     });
